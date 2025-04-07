cf018d43e8c173cae38482cec9de937e4713866c
jpbelval <jp@belval.org>
2025-04-07 10:41:02 -0400

    Add engine and xtime

---
+++ s4InfoAtelier4/bouya/src/engine.c
@@ -0,0 +1,14 @@
+#include "engine.h"
+#include "renderer.h"
+
+void start(){
+    static float gameTime = 0;
+    XTime tStart, tElapsedTime;
+
+
+    init_renderer();
+
+    while (1) {
+        elapsedTime += 
+    }
+}
---
+++ s4InfoAtelier4/bouya/src/engine.h
@@ -0,0 +1,14 @@
+#ifndef _ENGINE_H__
+#define _ENGINE_H__
+#include "player.h"
+#include "tilemap.h"
+#include "scaler.h"
+#include "renderer.h"
+#include "vdma.h"
+#include "xil_printf.h"
+#include "mycolorRegister.h"
+#include "xtime_1.h"
+
+void start();
+
+#endif
